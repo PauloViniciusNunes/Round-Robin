0842c7e5fd8508cd6c8468bba7406106687a47d5
pedronogueira-nunes <pedrohenriquenogueira268@gmail.com>
2026-04-22 09:28:31 -0300

    atualização 02

--- funcoes.c
+++ funcoes.c
@@ -11,7 +11,7 @@ typedef struct processos {
     int temp_disc;
     int temp_disc_rest;
 
-    int rodadas;
+    int rodadas_total;
     int rodadas_rest;
 
     int temp_espera;
@@ -35,8 +35,24 @@ void enfileirar(processos p) {
     fila[fim] = p;
     fim = (fim + 1) % MAX;
 }
+processos desenfileirar () {
+    processos p = fila[inicio];
+    inicio = (inicio + 1) % MAX;
+    return(p);
+}
+int vazio() {
+    return(inicio == fim);
+}
 
 void enfileirar_disc(processos p) {
     fila_disc[fim_disc] = p;
     fim_disc = (fim_disc + 1) % MAX;
+}
+processos desenfileirar_disc() {
+    processos p = fila_disc[inicio_disc];
+    inicio_disc = (inicio_disc + 1) % MAX;
+    return(p);
+}
+int vazio_disc() {
+    return(inicio_disc == fim_disc);
 }
